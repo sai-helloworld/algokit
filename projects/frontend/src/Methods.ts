ef6b3965252570f4133241e79c432efe66a871c3
Srikarnivas <srikarnivas.24@gmail.com>
2024-11-26 19:01:20 +0530

    restarted the front end

--- projects/frontend/src/Methods.ts
+++
@@ -1,8 +0,0 @@
-import * as algokit from '@algorandfoundation/algokit-utils'
-import { EscrowServiceClient } from './contracts/EscrowService'
-
-export function create(algorand: algokit.AlgorandClient, esClient: EscrowServiceClient, worker: string, adminAddress: string) {
-  return async () => {
-    await esClient.create.createApplication({ worker: worker, adminAddress: adminAddress })
-  }
-}
