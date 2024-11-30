b823c48383baadd3b3ad3bbe43468088e9c89115
Srikarnivas <srikarnivas.24@gmail.com>
2024-11-30 18:45:56 +0530

    added wallet address

--- projects/frontend/src/main.tsx
+++ projects/frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/main.css'
-import ErrorBoundary from './components/ErrorBoundary'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
